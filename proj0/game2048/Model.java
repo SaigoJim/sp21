89f72b57e838e9b25d081716c7395ad40b197a7b
SaigoJim <1533137229@qq.com>
2021-04-23 18:10:38 +0800

    implement emptySpaceExists and maxTileExists

--- proj0/game2048/Model.java
+++ proj0/game2048/Model.java
@@ -138,7 +138,14 @@ public class Model extends Observable {
      * */
     public static boolean emptySpaceExists(Board b) {
         // TODO: Fill in this function.
-
+        int size = b.size();
+        for (int i = 0; i < size; i += 1) {
+            for (int j = 0; j < size; j += 1) {
+                if (b.tile(j, i) == null) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
@@ -149,6 +156,15 @@ public class Model extends Observable {
      */
     public static boolean maxTileExists(Board b) {
         // TODO: Fill in this function.
+        int size = b.size();
+        for (int i = 0; i < size; i += 1) {
+            for (int j = 0; j < size; j += 1) {
+                Tile curTile = b.tile(j, i);
+                if (curTile != null && curTile.value() == MAX_PIECE) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
